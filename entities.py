ca06bd1cd7509bf13b2b7bdcc08f2f8948208a70
Ben <99620362+WrapEleven@users.noreply.github.com>
2023-07-10 15:46:16 -0400

    Add files via upload
    Branching algorithm prototype

--- entities.py
+++ entities.py
@@ -18,12 +18,11 @@ mentioned below and all CSC111 course staff at the University of Toronto.
 Any other parties not mentioned may not use or possess copies of
 this code, whether modified or otherwise.
 
-This file is Copyright (c) 2023 
+This file is Copyright (c) 2023.
 Jason Barahan, Vibhas Raizada, Benjamin Sandoval, Eleonora Scognamiglio.
 """
 from __future__ import annotations
-from typing import Optional, Any
-from python_ta.contracts import check_contracts
+from typing import Optional
 import math
 
 # Global variables
@@ -33,7 +32,7 @@ AMENITIES = {
 }
 
 
-@check_contracts
+# @check_contracts
 class Building:
     """
       A class for the buildings within the UofT campus.
@@ -61,7 +60,7 @@ class Building:
     def __init__(self, code: str, name: str, amenities: set[str],
                  coordinates: tuple[float, float]) -> None:
         """Initialize all attributes BUT the closest intersection. This is because
-            we will calculate the closest intersection when we import the CSVs."""
+        we will calculate the closest intersection when we import the CSVs."""
         self.code = code
         self.name = name
         self.closest_intersection = None
@@ -69,7 +68,7 @@ class Building:
         self.coordinates = coordinates
 
 
-@check_contracts
+# @check_contracts
 class Intersection:
     """
       A class for the intersections (nodes) within the UofT campus.
@@ -82,7 +81,8 @@ class Intersection:
       - edges: edges connected to this intersection
 
       Representation Invariants:
-       - self.close_buildings == set() or all(self.coordinates == b.closest_intersection.coordinates for b in self.close_buildings)
+       - self.close_buildings == set() or all(self.coordinates == b.closest_intersection.coordinates
+        for b in self.close_buildings)
       """
     identifier: int
     name: set[str]
@@ -93,20 +93,71 @@ class Intersection:
     def __init__(self, identifier: int, name: set[str],
                  coordinates: tuple[float, float]) -> None:
         """
-            Initialize an intersection object.
+        Initialize an intersection object.
 
-            close_buildings is EMPTY as we are determining the close buildings
-            after the fact.
-            edges is EMPTY as adjacent intersections will be determined after-the-fact.
-            """
+        close_buildings is EMPTY as we are determining the close buildings
+        after the fact.
+        edges is EMPTY as adjacent intersections will be determined after-the-fact.
+        """
         self.identifier = identifier
         self.name = name
         self.close_buildings = set()  # empty
         self.coordinates = coordinates
         self.edges = set()  # edges are empty
 
+    def find_all_paths(self, destination_id: int, visited: set[Intersection],
+                       max_distance: int) -> list[list[Edge]]:
+        """Finds all the paths from self to destination_id.
+        When you call this method for the first time, destination_id is the id of the node you want to get to and
+        visited should be an empty set."""
 
-@check_contracts
+        all_paths_so_far = []
+        self._find_all_paths_helper(destination_id, visited, [],
+                                    all_paths_so_far, max_distance)
+        return all_paths_so_far
+
+    def _find_all_paths_helper(self, destination_id: int,
+                               visited: set[Intersection],
+                               path_so_far: list[Edge],
+                               all_paths_so_far: list[list[Edge]],
+                               max_distance: int) -> None:
+        """Helper to find_all_paths"""
+
+        if self.identifier == destination_id:
+            all_paths_so_far.append(path_so_far)
+        else:
+            visited.add(self)
+
+            for edge in self.edges:
+                neighbour = edge.get_other_endpoint(self)
+                new_path_so_far = path_so_far.copy()
+                new_path_so_far.append(edge)
+
+                sum_so_far = sum(e.distance for e in new_path_so_far)
+
+                if neighbour not in visited and sum_so_far < max_distance:
+                    # Critical that a copy is passed into the recursive call.
+                    # Otherwise, the same visited set will be mutated and affect other recursive branches.
+                    # Same goes for a copy of path_so_far.
+                    neighbour._find_all_paths_helper(destination_id,
+                                                     visited.copy(),
+                                                     new_path_so_far,
+                                                     all_paths_so_far,
+                                                     max_distance)
+
+    def find_edge(self, other_intersection: Intersection) -> Optional[Edge]:
+        """Find the edge between self and other_intersection.
+
+        Preconditions:
+          - self and other_intersections are connected
+        """
+        for edge in self.edges:
+            if edge.get_other_endpoint(self).identifier == other_intersection.identifier:
+                return edge
+        return None  # this should never be reached
+
+
+# @check_contracts
 class Edge:
     """
       The 'roadway' connecting two Intersections.
@@ -125,13 +176,12 @@ class Edge:
     def __init__(self, intersection1: Intersection,
                  intersection2: Intersection) -> None:
         """
-            Initialize an edge object representing a connection between two nodes
-            (intersection or building). Edges are weighted to measure distance between.
-            """
+        Initialize an edge object representing a connection between two nodes
+        (intersection or building). Edges are weighted to measure distance between.
+        """
         self.endpoints = {intersection1, intersection2}
-        lat_1, long_1 = intersection1.coordinates
-        lat_2, long_2 = intersection2.coordinates
-        distance = math.dist([lat_1, long_1], [lat_2, long_2])
+        distance = get_distance(intersection1.coordinates,
+                                intersection2.coordinates)
         self.distance = distance
 
     def get_other_endpoint(self, intersection: Intersection) -> Intersection:
@@ -143,7 +193,7 @@ class Edge:
         return (self.endpoints - {intersection}).pop()
 
 
-@check_contracts
+# @check_contracts
 class AbstractGrid:
     """
     The map of U of T.
@@ -164,9 +214,9 @@ class AbstractGrid:
     def find_closest_intersection(self, building_code: str) -> int:
         """Finds the intersection with the closest Euclidean distance to that of the building.
         Used for the initialization of all buildings.
-            Input: The building code.
-            Output: The ID of the intersection.
-            """
+        Input: The building code.
+        Output: The ID of the intersection.
+        """
         closest_so_far = None
         closest_distance = None
         for identifier, intersection in self.intersections.items():
@@ -176,9 +226,6 @@ class AbstractGrid:
                 coordinates2 = self.buildings[building_code].coordinates
                 closest_distance = get_distance(coordinates1, coordinates2)
             else:
-                # lat_1, long_1 = intersection.coordinates
-                # lat_2, long_2 = self.buildings[building_code].coordinates
-                # distance = math.dist([lat_1, long_1], [lat_2, long_2])
                 coordinates1 = intersection.coordinates
                 coordinates2 = self.buildings[building_code].coordinates
                 distance = get_distance(coordinates1, coordinates2)
@@ -200,16 +247,15 @@ class AbstractGrid:
 
         return buildings_so_far
 
-    def find_shortest_path(self, id1: int, id2: int) -> list[Edge]:
-        """Retturn the shortest path between the two given intersections
+    def find_shortest_path(self, id1: int,
+                           id2: int) -> list[Intersection]:
+        """Return the shortest path between the two given intersections
 
         Preconditions:
-         - ... TODO
+         - id1 in self.intersections
+         - id2 in self.intersections
         """
         raise NotImplementedError
-
-
-# TODO: introduce an unimplemented method so it is abstract
 
 
 def get_distance(p1: tuple[float, float], p2: tuple[float, float]) -> float:
@@ -223,40 +269,22 @@ def get_distance(p1: tuple[float, float], p2: tuple[float, float]) -> float:
     lat2_rad = math.radians(lat2)
     long1_rad = math.radians(long1)
     long2_rad = math.radians(long2)
-    r = 6.371 * (10**6)  # Earth radius
-    term1 = math.sin((lat2_rad - lat1_rad) / 2)**2
-    term2 = (math.sin((long2_rad - long1_rad) / 2)**
-             2) * math.cos(lat1_rad) * math.cos(lat2_rad)
+    r = 6.371 * (10 ** 6)  # Earth radius
+    term1 = math.sin((lat2_rad - lat1_rad) / 2) ** 2
+    term2 = (math.sin((long2_rad - long1_rad) / 2) ** 2) * math.cos(lat1_rad) * math.cos(lat2_rad)
     h = term1 + term2
     d = 2 * r * math.asin(math.sqrt(h))
     return d
 
 
-# # BUILDING SELECTION TOOLS
-# def select_building_with_amenity(location: ent.Building | ent.Intersection, amenity: str) -> ent.Building:
-#     """
-#     Selects the closest building with a specified amenity.
-#     """
-#
-#
-# # PATH GENERATION TOOLS
-# def get_shortest_path(start: ent.Building,
-#                       end: ent.Building) -> dict[ent.Building | ent.Intersection]:
-#     """
-#     Generate the shortest path between two nodes.
-#     In this case, paths are collections of nodes.
-#     """
-#
-#
-# def get_path_includes_node(start: ent.Building, end: ent.Building, includes: ent.Building) -> dict[
-#     ent.Building | ent.Intersection]:
-#     """
-#     Generate the shortest path between two nodes that INCLUDES a certain node.
-#     This can be an endpoint or an intermediary node.
-#     """
-#
-#
-# def AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA(aaa: int, aaaa: int) -> int:
-#     """
-#     Procrastinate
-#     """
+if __name__ == '__main__':
+    import doctest
+
+    doctest.testmod()
+
+    import python_ta
+
+    python_ta.check_all(config={
+        'max-line-length': 120,
+        'disable': ['E9992', 'E9997', 'E9999', 'R0913']
+    })
